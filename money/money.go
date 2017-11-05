41d56d370e0c771d71f34d02b13cd5c7fb63803e
Kenta SUZUKI <suzuken326@gmail.com>
2017-11-05 09:52:49 +0900

    test passed

--- money/money.go
+++ money/money.go
@@ -4,5 +4,11 @@ type Dollar struct {
 	amount int
 }
 
+func NewDollar(amount int) *Dollar {
+	return &Dollar{
+		amount: 10,
+	}
+}
+
 func (d *Dollar) times(multiplier int) {
 }
--- money/money_test.go
+++ money/money_test.go
@@ -5,7 +5,7 @@ import (
 )
 
 func TestMoneyMultiplication(t *testing.T) {
-	five := Dollar{5}
+	five := NewDollar(5)
 	five.times(2)
 	if five.amount != 10 {
 		t.Errorf("want 10, got %d", five.amount)
