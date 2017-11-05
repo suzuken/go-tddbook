f9bf7a2c28e7e7820fb17f5718c606a06e90be3f
Kenta SUZUKI <suzuken326@gmail.com>
2017-11-05 11:36:38 +0900

    passed

--- money/money.go
+++ money/money.go
@@ -15,5 +15,6 @@ func (d *Dollar) times(multiplier int) *Dollar {
 }
 
 func (d *Dollar) equals(object interface{}) bool {
-	return true
+	dollar := object.(Dollar)
+	return d.amount == dollar.amount
 }
